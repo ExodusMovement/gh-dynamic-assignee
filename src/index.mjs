b62374cc0a8123104c1c2631095c98990a89bb44
R. Douglas Barbieri <doug@dooglio.net>
2022-07-26 17:21:01 -0300

    feat: improve logging to see structure of files in commit

--- src/index.mjs
+++ src/index.mjs
@@ -3,12 +3,24 @@ import * as github from '@actions/github'
 
 async function run() {
   try {
-    // const token = core.getInput('github_token') || process.env.GITHUB_TOKEN
-    // const octokit = new github.getOctokit(token)
-    console.log('github.context=', github.context)
-    console.log('github.context.repo=', github.context.repo)
+    // console.log('github.context=', github.context)
 
-    // core.setOutput(id)
+    const { eventName, sha, repo, payload } = github.context.repo
+    console.info('github.context.repo=', repo)
+    console.info('payload=', payload)
+
+    if (['pull_request', 'push'].includes(eventName)) {
+      const token = core.getInput('github_token') || process.env.GITHUB_TOKEN
+      const octokit = new github.getOctokit(token)
+
+      const res = await octokit.request(
+        `GET /repos/ExodusMovement/${repo.owner}/${repo.repo}/commits/${sha}`
+      )
+
+      console.info('res=', res)
+
+      // core.setOutput(id)
+    }
   } catch (err) {
     core.setFailed(err.message)
   }
