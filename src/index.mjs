fda457d2dee656e929650313e080691feee3c959
R. Douglas Barbieri <doug@dooglio.net>
2022-07-27 16:08:26 -0300

    build: look at path

--- src/index.mjs
+++ src/index.mjs
@@ -5,19 +5,21 @@ async function run() {
   try {
     console.log('github.context=', github.context)
 
-    const { eventName, sha, owner, repo, payload } = github.context
+    const { eventName, sha, owner, repo } = github.context
     console.info('eventName=', eventName)
     console.info('sha=', sha)
     console.info('owner=', owner)
     console.info('repo=', repo)
-    console.info('payload=', payload)
+    // console.info('payload=', payload)
 
     if (['pull_request', 'push'].includes(eventName)) {
       const token = core.getInput('github_token') || process.env.GITHUB_TOKEN
       const octokit = new github.getOctokit(token)
 
-      const res = await octokit.request(`GET /repos/ExodusMovement/${owner}/${repo}/commits/${sha}`)
+      const path = `GET /repos/ExodusMovement/${owner}/${repo}/commits/${sha}`
+      console.info('path=', path)
 
+      const res = await octokit.request(path)
       console.info('res=', res)
 
       // core.setOutput(id)
